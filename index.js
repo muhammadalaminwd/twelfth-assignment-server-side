da3af36af5e0bc0107f86bd4cf4bd2dfded5eaa1
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-24 17:53:02 +0600

    env & git ignore file created

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
 
-
 // middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
