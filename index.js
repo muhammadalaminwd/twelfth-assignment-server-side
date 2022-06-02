05bed90d3406f17f5a0277466c787c3acb26c5df
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-06-02 11:27:51 +0600

    updated cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { query } = require("express");
 const res = require("express/lib/response");
 
 // middleware
-app.use(cors());
+app.use(cors({origin: 'https://twelfth-assignment-a6d33.web.app/'}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vjkoe.mongodb.net/?retryWrites=true&w=majority`;
